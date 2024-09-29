8c55483aab03751ea0e00ed61b2db8f61fcdfc0e
YY018 <yashyadav752500@gmail.com>
2024-09-29 23:07:13 +0530

    Update Token.sol

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,32 +1,26 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.18;
 
 contract MyToken {
-    // Public variables for token details
-    string public name;
-    string public symbol;
-    uint256 public totalSupply;
 
-    // Mapping from address to balance
-    mapping(address => uint256) public balanceOf;
+    // Public variables to store details about the coin
+    string public name = "MyToken";           // Token Name
+    string public symbol = "MTK";             // Token Abbreviation
+    uint256 public totalSupply;               // Total Supply
 
-    // Constructor to set token details at deployment
-    constructor(string memory _name, string memory _symbol) {
-        name = _name;
-        symbol = _symbol;
-        totalSupply = 0; // Initial total supply is 0
-    }
+    // Mapping from addresses to balances
+    mapping(address => uint256) public balances;
 
-    // Mint function to create new tokens
+    // Mint function to increase total supply and balance of the given address
     function mint(address _to, uint256 _value) public {
-        totalSupply += _value;
-        balanceOf[_to] += _value;
+        totalSupply += _value;                // Increase total supply
+        balances[_to] += _value;              // Increase balance of the recipient
     }
 
-    // Burn function to destroy tokens
+    // Burn function to decrease total supply and balance of the given address
     function burn(address _from, uint256 _value) public {
-        require(balanceOf[_from] >= _value, "Insufficient balance to burn");
-        totalSupply -= _value;
-        balanceOf[_from] -= _value;
+        require(balances[_from] >= _value, "Insufficient balance to burn");  // Ensure the sender has enough balance
+        totalSupply -= _value;                 // Decrease total supply
+        balances[_from] -= _value;             // Decrease balance of the sender
     }
 }
